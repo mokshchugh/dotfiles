431cb586b2a16555ad4c3eab5d332a10cb0efc48
mokshchugh <mokshchugh302@gmail.com>
2026-02-02 03:32:36 +0530

    Initial dotfiles (Hyprland Setup)

---
+++ spyder-py3/history.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Jan 28 08:08:04 2026)---
+%runfile /home/moksh/.config/spyder-py3/temp.py --wdir
+
+## ---(Wed Jan 28 08:18:53 2026)---
+%runfile /home/moksh/.config/spyder-py3/temp.py --wdir
+
+## ---(Wed Jan 28 08:40:02 2026)---
+%runfile /home/moksh/.config/spyder-py3/temp.py --wdir
---
+++ spyder-py3/temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+
+image = cv2.imread('/home/moksh/Wallpapers/neon.png')
+print(image.shape)
